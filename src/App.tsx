539cb8fb11c5ec6e36b0bbdbcd9fcf9f7940f0da
Petros <petrosluros@gmail.com>
2025-11-14 09:35:03 +0200

    Fix React Router basename

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,12 @@ import {BrowserRouter, Routes, Route} from "react-router";
 import Layout from "@/components/layout/Layout.tsx";
 import HomePage from "@/components/pages/HomePage.tsx";
 
-
 import {Toaster} from "sonner";
-
 
 function App() {
     return (
         <>
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.MODE === 'production' ? '/my-portfolio' : '/'}>
                 <Routes>
                     <Route element={<Layout />}>
                         {/* Portfolio Routes */}
